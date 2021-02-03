575b29a96955ffcf994dacbbc856ea88150a74f2
eric <polvu@hotmail.fr>
2021-02-03 15:34:48 +0100

    test de déploiement sur Heroku

--- public/js/payment.js
+++ public/js/payment.js
@@ -5,7 +5,8 @@ const stripe = Stripe(stripePublicKey);
 const elements = stripe.elements();
 
 const card = elements.create("card");
-/*
+// on ne s'occupe pas du style pour l'instant
+
 const style = {
 base: {
 color: "#32325d",
@@ -22,26 +23,27 @@ color: "#fa755a",
 iconColor: "#fa755a"
 }
 };
-*/
 
-// Stripe injects an iframe into the DOM
+
+// Stripe injection de l'iframe dans le DOM
 card.mount("#card-element");
-card.on("change", function (event) { // Disable the Pay button if there are no card details in the Element
+card.on("change", function (event) { // Désactiver le bouton si il n'y a pas de détail sur le paiement
 document.querySelector("button").disabled = event.empty;
 document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
 });
 const form = document.getElementById("payment-form");
 form.addEventListener("submit", function (event) {
 event.preventDefault();
-// Complete payment when the submit button is clicked
+// Completer le paiment si le bouton submit est cliqué
 stripe.confirmCardPayment(clientSecret, {
 payment_method: {
 card: card
 }
 }).then(function (result) {
-if (result.error) { // Show error to your customer
+if (result.error) { // Si il y a une erreur la montrer a l'utilisateur
 console.log(result.error.message);
-} else { // The payment succeeded!
+} else { // Le paiement est un succès
+    // voir les Webhooks de stripe pour que la commande soit mise en payée méme si la redirection ne fonctionne pas
 window.location.href = redirectAfterSuccessUrl;
 }
 });
